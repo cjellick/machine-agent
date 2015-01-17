1c38705cd47b8c6761b210f2203a295445eee430
Craig Jellick <craig@rancher.com>
2015-01-16 22:04:56 -0700

    Reorganizing code

--- locks/locks.go
+++ locks/locks.go
@@ -1,18 +1,24 @@
 package locks
 
+var lockRequests chan *lockRequest
+
+func init() {
+	lockRequests = make(chan *lockRequest)
+	go locker()
+}
+
+// Interface for unlocking a key that was provided to Lock()
 type Unlocker interface {
 	Unlock()
 }
 
+// Provides an application-wide lock on the provided key.
+// If the lock is obtained, it will return an Unlocker.
+// If the lock is not successfully obtained, nil will be returned.
 func Lock(key string) Unlocker {
 	lockRequest := newLockRequest(key, LOCK)
 	lockRequests <- lockRequest
-	success := <-lockRequest.success
-	if success {
-		return newUnlocker(key)
-	}
-
-	return nil
+	return <-lockRequest.response
 }
 
 type operation int
@@ -23,45 +29,16 @@ const (
 )
 
 type lockRequest struct {
-	key     string
-	op      operation
-	success chan bool
-}
-
-var lockRequests chan *lockRequest
-
-func locker() {
-	// note: bool value is meaningless. This is a set
-	lockedItems := make(map[string]bool)
-	for {
-		lockReq := <-lockRequests
-		switch lockReq.op {
-		case LOCK:
-			if _, locked := lockedItems[lockReq.key]; locked {
-				// already locked
-				lockReq.success <- false
-			} else {
-				lockedItems[lockReq.key] = true
-				lockReq.success <- true
-			}
-		case UNLOCK:
-			delete(lockedItems, lockReq.key)
-			lockReq.success <- true
-		}
-	}
-}
-
-func init() {
-	lockRequests = make(chan *lockRequest)
-	go locker()
-
+	key      string
+	op       operation
+	response chan Unlocker
 }
 
 func newLockRequest(key string, op operation) *lockRequest {
 	return &lockRequest{
-		key:     key,
-		op:      op,
-		success: make(chan bool, 1),
+		key:      key,
+		op:       op,
+		response: make(chan Unlocker, 1),
 	}
 }
 
@@ -75,4 +52,23 @@ func (u *unlockerImpl) Unlock() {
 
 func newUnlocker(key string) *unlockerImpl {
 	return &unlockerImpl{key: key}
+}
+
+func locker() {
+	// note: bool value is meaningless. This is a set
+	lockedItems := make(map[string]bool)
+	for {
+		lockReq := <-lockRequests
+		switch lockReq.op {
+		case LOCK:
+			if _, locked := lockedItems[lockReq.key]; locked {
+				lockReq.response <- nil
+			} else {
+				lockedItems[lockReq.key] = true
+				lockReq.response <- newUnlocker(lockReq.key)
+			}
+		case UNLOCK:
+			delete(lockedItems, lockReq.key)
+		}
+	}
 }
